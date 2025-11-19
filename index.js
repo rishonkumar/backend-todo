9e5ccdb24a4bd504999bf784f3d555ede810e178
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-11-19 09:20:34 +0530

    Added api

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-const express = require("express")
+const express = require("express");
+const { createdTodo } = require("./types");
+const { todo } = require("./db");
 const app = express()
 const PORT = 3000;
 app.use(express.json())
 
 
-app.post("/todo", (req,res) => {
+app.post("/todo", async (req,res) => {
+    const userPayload = req.body
+    const parsedUserPayload = createdTodo.safeParse(userPayload)
 
+    if(!parsedUserPayload.success){
+        res.status(411).json({
+            msg : "You send the wrong input"
+        })
+        return;
+    }
+
+    await todo.create({
+        title : userPayload.title,
+        description : userPayload.description,
+        completed : false
+    })
+
+    res.json({
+        msg : "todo created"
+    })
 })
 
-app.get("/todos", (req,res) => {
+app.get("/todos", async (req,res) => {
 
+    const todos = await todo.find();
+
+    res.json({todos})
 })
 
+app.get("/completed", async (req,res) => {
+    const userPayload = req.body
+    const parsedUserPayload = createdTodo.safeParse(userPayload)
+
+    if(!parsedUserPayload.success) {
+        res.status(411).json({
+            msg : "You send the wrong input"
+        })
+        return;
+    }
+    await todo.updateOne({
+        _id: req.body.id, // reason for _id in mongoose idenify
+    },{
+        completed : true
+    })
+
+    res.json({
+        msg : "Marked as completed"
+    })
+})
 
 app.listen(PORT, function started(){
     console.log("App started ", PORT);
