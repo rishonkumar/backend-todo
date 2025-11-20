93f6051b819e622c06b60ae332e40fc2b12d4f60
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-11-20 11:27:35 +0530

    Adding  cors

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("");
+mongoose.connect("mongodb://localhost:27017/todo");
 
 const todoSchema = mongoose.Schema({
-    title : String,
-    description : String,
-    completed : Boolean
+    title: String,
+    description: String,
+    completed: Boolean
 })
 
 const todo = mongoose.model('todos', todoSchema)
 
-module.exports= {todo}
+module.exports = { todo }
--- index.js
+++ index.js
@@ -3,60 +3,62 @@ const { createdTodo } = require("./types");
 const { todo } = require("./db");
 const app = express()
 const PORT = 3000;
+const cors = require("cors")
+
 app.use(express.json())
+app.use(cors())
 
-
-app.post("/todo", async (req,res) => {
+app.post("/todo", async (req, res) => {
     const userPayload = req.body
     const parsedUserPayload = createdTodo.safeParse(userPayload)
 
-    if(!parsedUserPayload.success){
+    if (!parsedUserPayload.success) {
         res.status(411).json({
-            msg : "You send the wrong input"
+            msg: "You send the wrong input"
         })
         return;
     }
 
     await todo.create({
-        title : userPayload.title,
-        description : userPayload.description,
-        completed : false
+        title: userPayload.title,
+        description: userPayload.description,
+        completed: false
     })
 
     res.json({
-        msg : "todo created"
+        msg: "todo created"
     })
 })
 
-app.get("/todos", async (req,res) => {
+app.get("/todos", async (req, res) => {
 
     const todos = await todo.find();
 
-    res.json({todos})
+    res.json({ todos })
 })
 
-app.get("/completed", async (req,res) => {
+app.get("/completed", async (req, res) => {
     const userPayload = req.body
     const parsedUserPayload = createdTodo.safeParse(userPayload)
 
-    if(!parsedUserPayload.success) {
+    if (!parsedUserPayload.success) {
         res.status(411).json({
-            msg : "You send the wrong input"
+            msg: "You send the wrong input"
         })
         return;
     }
     await todo.updateOne({
         _id: req.body.id, // reason for _id in mongoose idenify
-    },{
-        completed : true
+    }, {
+        completed: true
     })
 
     res.json({
-        msg : "Marked as completed"
+        msg: "Marked as completed"
     })
 })
 
-app.listen(PORT, function started(){
+app.listen(PORT, function started() {
     console.log("App started ", PORT);
-    
+
 })
